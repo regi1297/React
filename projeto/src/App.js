caf36882cbe82606e87495e7168d8a3ec346a39d
regi1297 <reginald_miranda@hotmail.com>
2022-11-27 10:41:06 -0300

    shekel of life using components

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,44 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
 import { Component } from 'react';
+import './App.css';
 
 class App extends Component {
  state = {
-   name: 'Reginaldo',
-   counter: 0
+  counter: 0,
+  posts: [
+    {
+      id: 1,
+      title: 'O título 1',
+      body: 'O corpo 1',
+    },
+    {
+      id: 2,
+      title: 'O título 2',
+      body: 'O corpo 2',
+    },
+    {
+      id: 3,
+      title: 'O título 3',
+      body: 'O corpo 3',
+    },
+  ]
   };
 
-handlePClick = () => {
-  this.setState({ name: 'Junior'})
+timeoutUpdate = null;
+
+componentDidMount () {
+  this.handleTimeout();  
 }
 
-handleAClick = (event) => {
-  event.preventDefault();
-  const { counter } =  this.state;
-    this.setState({ counter: counter + 1});
+componentDidUpdate() {
+  this.handleTimeout();
 }
 
+componentWillUnmount() {
+  clearTimeout(this.timeoutUpdate);
+}
 
+handleTimeout = () => {
+  const { posts, counter } = this.state;
+  posts[0].title = 'O titulo mudou';
+
+  this.timeoutUpdate = setTimeout(() => {
+    this.setState({ posts, counter: counter + 1 });
+
+  }, 1000);
+}
   render () {
-      const { name, counter } = this.state;
+      const { posts, counter } = this.state;
 
   return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={this.handlePClick}>
-          {name} {counter}
-        </p>
-        <a
-        onClick={this.handleAClick}
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Este é o link
-        </a>
-      </header>
+      <p>{counter}</p>
+      {posts.map(post => (
+        <div key={post.id}>
+         <h1>{post.title}</h1>
+         <p>{post.body}</p>
+        </div>
+      ))}
     </div>
   );
 }
