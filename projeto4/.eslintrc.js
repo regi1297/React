606509058fb1104ffde45273d34713579ee20d4e
regi1297 <reginald_miranda@hotmail.com>
2023-01-08 18:54:16 -0300

    Project 4 initial settings

---
+++ projeto4/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
